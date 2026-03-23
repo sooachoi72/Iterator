84b7027efcbda20dfc48ae186caa02f21ed61558
sooachoi72 <sooachoi72@users.noreply.github.com>
2026-03-24 01:57:51 +0900

    주석 수정

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
         // [Step 4-1] 10개의 책 데이터를 담을 수 있도록 크기 지정 및 데이터 추가
         BookShelf bookShelf = new BookShelf(10);
         
-        // PDF 테이블 데이터 입력 [cite: 39, 40]
+        // PDF 테이블 데이터 입력
         bookShelf.appendBook(new Book("클린코드", "기술", 2008, 30000));
         bookShelf.appendBook(new Book("해리포터와 마법사의 돌", "소설", 1997, 15000));
         bookShelf.appendBook(new Book("사피엔스", "역사", 2011, 18000));
@@ -23,14 +23,14 @@ public class Main {
         bookShelf.appendBook(new Book("로마인 이야기", "역사", 1992, 25000));
         bookShelf.appendBook(new Book("어린왕자", "소설", 1943, 9000));
 
-        // 기존 코드와의 호환성 테스트 (Step 1 생성자 오버로딩 확인) [cite: 20]
+        // 기존 코드와의 호환성 테스트 (Step 1 생성자 오버로딩 확인)
         System.out.println("----- 기존 방식 순회 -----");
         for (Book book : bookShelf) {
             System.out.println(book.getName());
         }
         System.out.println();
 
-        // [Step 4-2] 장르 필터 Iterator 테스트 ('소설' 장르만 출력) [cite: 41, 42]
+        // [Step 4-2] 장르 필터 Iterator 테스트 ('소설' 장르만 출력)
         System.out.println("----- 장르 필터 테스트: 소설 -----");
         Iterator<Book> genreIt = bookShelf.iteratorByGenre("소설");
         while (genreIt.hasNext()) {
@@ -40,7 +40,7 @@ public class Main {
         }
         System.out.println();
 
-        // [Step 4-3] 출판연도 역순 Iterator 테스트 (최신순 출력) [cite: 43, 44]
+        // [Step 4-3] 출판연도 역순 Iterator 테스트 (최신순 출력)
         System.out.println("----- 출판연도 역순 테스트 -----");
         Iterator<Book> yearIt = bookShelf.iteratorByYear();
         while (yearIt.hasNext()) {
